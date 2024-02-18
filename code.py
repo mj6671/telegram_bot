2718e60a4ae6b69141745556e6367c505e499f58
Mahendran <98400425+mj6671@users.noreply.github.com>
2024-02-18 11:34:43 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,56 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import data as tt
+
+TOKEN: Final = '6142493838:AAHgItNj9JrTVqUMbrurKWnmy0DYcAfiHZ8'
+BOT_USERNAME: Final = '@bodysoda'
+
+def handle_response(text: str) -> str:
+    result = tt.bot(text)
+    return result
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    response: str = handle_response(text)
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! Thanks for chatting with me! I am here to help you with your exams!')
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is a custom command!')
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_error_handler(error)
+    print('Polling…..')
+    app.run_polling(poll_interval=3)
+
+          
+          
+                
+          
+          
+         
+          
+
+
+
+
+
+
+
+
 
